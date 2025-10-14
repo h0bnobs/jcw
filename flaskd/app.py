cf086b656a357df629c2bcf5df3404085f95c6bd
h0bnobs <mb8ld0ck@gmail.com>
2025-10-14 19:10:31 +0100

    chore: added active download page so user can see download progress

--- flaskd/app.py
+++ flaskd/app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO
 from src.qbt.download_torrent import download_torrent
 from src.qbt.find_torrents import get_torrents
+from src.qbt.torrent_download_status import get_active_downloads
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 app.secret_key = 'your_secret_key'
@@ -34,4 +35,46 @@ def download():
 
 @app.route('/downloads', methods=['GET', 'POST'])
 def downloads():
-    
+    active_downloads = get_active_downloads()
+    # filter out irrelevant info, and convert to clearer values
+    # speed: 9384459
+    # progress: 0.5536684098026253
+    # eta: 33
+    active_downloads = [
+        {
+            'content_path': d['content_path'],
+            'dlspeed': int(d['dlspeed']) / 1000000,
+            'eta': int(d['eta']),
+            'progress': round(float(d['progress']) * 100, 0),
+        }
+        for d in get_active_downloads()
+    ]
+
+    return render_template('downloads.html', active_downloads=active_downloads)
+
+from threading import Thread, Event
+
+thread = None
+thread_stop_event = Event()
+
+def background_download_status():
+    """Send active downloads to all clients every 1 second."""
+    while not thread_stop_event.is_set():
+        active_downloads = [
+            {
+                'content_path': d['content_path'],
+                'dlspeed': int(d['dlspeed']) / 1000000,
+                'eta': int(d['eta']),
+                'progress': round(float(d['progress']) * 100, 0),
+            }
+            for d in get_active_downloads()
+        ]
+        socketio.emit('update_downloads', active_downloads)
+        socketio.sleep(1)  # sleep 1s between updates
+
+
+@socketio.on('connect')
+def handle_connect():
+    global thread
+    if thread is None:
+        thread = socketio.start_background_task(background_download_status)
--- src/qbt/torrent_download_status.py
+++ src/qbt/torrent_download_status.py
@@ -3,4 +3,5 @@ from qbittorrent import Client
 qb = Client('http://localhost:9000/')
 qb.login()
 
-def get_all_torrent_download_status():
+def get_active_downloads() -> list:
+    return qb.torrents(filter='downloading')
