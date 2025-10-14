d2a026a9e28e6f0b4d98ab6e957c7738e4175462
h0bnobs <mb8ld0ck@gmail.com>
2025-10-14 20:11:49 +0100

    chore: readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # jcw
+
+Simple torrenting portal.
+
+### Prerequisites
+- [qBittorrent](https://www.qbittorrent.org/) + [qBittorrent WebUI enabled](https://github.com/lgallard/qBittorrent-Controller/wiki/How-to-enable-the-qBittorrent-Web-UI)
+- VPN from any major provider
--- src/tests/torrent_search.py
+++ src/tests/torrent_search.py
@@ -1,8 +1,14 @@
 import unittest
 
+import requests
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_piratebay_resolve(self):
+        search_url = f"https://thepiratebay10.org/search/cars/1/99/0"
+        response = requests.get(search_url)
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
